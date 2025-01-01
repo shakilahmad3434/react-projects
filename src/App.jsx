c9c87c9a4dcf578c6eb549e87ba6f8064529eda1
shakilahmad3434 <shakilahmad342100@gmail.com>
2025-01-01 13:44:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HolidayForm from "./components/HolidayForm";
+import 'remixicon/fonts/remixicon.css'
+
+const App = () => {
+  return(
+
+        <HolidayForm />
+
+  )
+}
+
+export default App
